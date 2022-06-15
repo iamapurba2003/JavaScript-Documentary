5aab6abf5c6304114f3685b73047ebf05849872b
iamapurba2003 <new.to.world2003@gmail.com>
2022-06-16 01:43:51 +0530

    Half Way There .. COmpleted Basics of Variables, Contants, OPerators, Strings &n Concepts like String Interpolation/Template Literals etc. More to Cover... @iamapurba2003

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,15 @@
-alert("This Works!!")
+let defaultResult = 0;
+let currentResult = defaultResult;
+
+currentResult = (currentResult + 10) * 3 / 2 - 1;
+
+// let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1';
+// or
+
+let calculationDescription = `( ${defaultResult} +   
+10) * 3 / 2 - 1`;
+
+let errorMessege = 'An Error \n' +  
+'Occured';
+
+outputResult(currentResult, errorMessege);
