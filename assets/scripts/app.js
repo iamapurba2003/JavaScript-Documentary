572fe04029903f1d4b7d24be17f0dd7d5da3408c
iamapurba2003 <new.to.world2003@gmail.com>
2022-06-16 15:44:49 +0530

    Completed the Functionality of the Calculator. The Basic Concepts of Functions & Operators are done. Also, some Best Practices are also followed'

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,15 +1,47 @@
 let defaultResult = 0;
 let currentResult = defaultResult;
 
-currentResult = (currentResult + 10) * 3 / 2 - 1;
+function getUserNumberInput() {
+	return parseInt(userInput.value);
+}
+
+function createAndWriteLog(operator, resultBeforeCalc, calcNumber) {
+	const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+	outputResult(currentResult, calcDescription);
+}
+
+function addNumbers() {
+	const enteredNumber = getUserNumberInput();
+	const initialResult = currentResult;
+	currentResult += enteredNumber;
+	createAndWriteLog("+", initialResult, enteredNumber);
+}
+
+function subtractNumber() {
+	const enteredNumber = getUserNumberInput();
+	const initialResult = currentResult;
+	currentResult -= enteredNumber;
+	createAndWriteLog("-", initialResult, enteredNumber);
+}
+
+function multiplyNumber() {
+	const enteredNumber = getUserNumberInput();
+	const initialResult = currentResult;
+	currentResult *= enteredNumber;
+	createAndWriteLog("*", initialResult, enteredNumber);
+}
+
+function divideNumber() {
+	const enteredNumber = getUserNumberInput();
+	const initialResult = currentResult;
+	currentResult /= enteredNumber;
+	createAndWriteLog("/", initialResult, enteredNumber);
+}
 
 // let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1';
 // or
 
-let calculationDescription = `( ${defaultResult} +   
-10) * 3 / 2 - 1`;
-
-let errorMessege = 'An Error \n' +  
-'Occured';
-
-outputResult(currentResult, errorMessege);
+addBtn.addEventListener("click", addNumbers);
+subtractBtn.addEventListener("click", subtractNumber);
+multiplyBtn.addEventListener("click", multiplyNumber);
+divideBtn.addEventListener("click", divideNumber);
